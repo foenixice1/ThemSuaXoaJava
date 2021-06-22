a53b18a11403ea9fc1f58f93be1970d0da35f9fd
foenixice1 <foenixice1@gmail.com>
2021-06-22 10:21:44 +0700

    th

---
+++ TestMap/TestMap.java
@@ -0,0 +1,29 @@
+package TestMap;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TestMap {
+    public static void main(String[] args) {
+        Map<String, Integer> hasMap = new HashMap<>();
+        hasMap.put("Nam", 22);
+        hasMap.put("Hien", 21);
+        hasMap.put("Hung", 20);
+        hasMap.put("Hoang", 19);
+        System.out.println("Cac muc trong HasMap");
+        System.out.println(hasMap + "\n");
+
+        Map<String, Integer> treeMap = new TreeMap<>();
+        System.out.println("Cac muc trong TreeMap");
+        System.out.println(treeMap);
+
+        Map<String, Integer> linkedHasMap = new LinkedHashMap<>(16, 0.75f, true);
+        linkedHasMap.put("Nam" , 22);
+        linkedHasMap.put("Hien" , 21);
+        linkedHasMap.put("Hung" , 20);
+        linkedHasMap.put("Hoang" , 19);
+        System.out.println("Tuoi cua Nam la : " + linkedHasMap.get("Nam"));
+    }
+}
--- ThapPhanNhiPhanStack/DecimalToBinaryExample.java
+++ ThapPhanNhiPhanStack/DecimalToBinaryExample.java
@@ -1,40 +1,27 @@
 package ThapPhanNhiPhanStack;
+
 import java.util.Stack;
 
 public class DecimalToBinaryExample {
-    public void convertBinary(int num){
+    public void convertBinary(int num) {
 
         Stack<Integer> stack = new Stack<Integer>();
 
-        while(num != 0){
-
-            //thực hiện phép chia lấy phần dư cho 2.
-
-            int d = num%2;
-
-            // thêm vào stack.
-
+        while (num != 0) {
+            int d = num % 2;
             stack.push(d);
-
-            num/=2;
-
+            num /= 2;
         }
-
-        while(!(stack.isEmpty())){
-
+        while (!(stack.isEmpty())) {
             System.out.print(stack.pop());
-
         }
-
-
     }
 
-
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         int decimalNumber = 123456789;
 
-        System.out.print("Hệ nhị phân của " + decimalNumber +" là :");
+        System.out.print("Hệ nhị phân của " + decimalNumber + " là :");
 
         new DecimalToBinaryExample().convertBinary(decimalNumber);
 
