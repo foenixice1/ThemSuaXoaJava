99fa44c24df56fe7a00b65cdf41f7c87b124d917
foenixice1 <foenixice1@gmail.com>
2021-06-24 07:58:12 +0700

    bt

---
+++ QLNhaDat/Dat.java
@@ -0,0 +1,22 @@
+package QLNhaDat;
+
+public class Dat extends GiaoDich{
+
+    public Dat(){}
+
+    public Dat(int maGiaoDich, String ngayGiaoDich, int donGia, int dienTich, String phanLoai) {
+        super(maGiaoDich, ngayGiaoDich, donGia, dienTich, phanLoai);
+    }
+
+    @Override
+    public String toString() {
+        return "Dat{" +
+                "loaiDat='" +  '\'' +
+                ", maGiaoDich=" + maGiaoDich +
+                ", ngayGiaoDich='" + ngayGiaoDich + '\'' +
+                ", donGia=" + donGia +
+                ", dienTich=" + dienTich +
+                '}';
+    }
+}
+
